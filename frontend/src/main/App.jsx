eba9fd2ef42e746a4510668ae9d56794f6055262
WellintonCamboim <wellinton.camboim@gmail.com>
2020-05-24 22:51:49 -0300

    Componente Conteúdo(Main)

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import '../components/home/Home'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
+import Home from '../components/home/Home';
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Início" subtitle="Projeto de Cadastro de Usuário - CRUD-REACT"/>
+        <Home />
         <Footer />
     </div>
