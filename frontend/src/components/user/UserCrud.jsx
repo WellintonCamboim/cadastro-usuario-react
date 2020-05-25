ad981ec9d82094bf84eaf5a6045ceee782d5c39b
WellintonCamboim <wellinton.camboim@gmail.com>
2020-05-25 16:47:47 -0300

    Componente Cadastro Usuário(save and clear)

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -38,10 +38,63 @@ export default class UserCrud extends Component {
         return list
     }
 
+    // Alterar um usuário
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome: </label>
+                            <input type="text" className="form-control"
+                            name="name"
+                            value={this.state.user.name}
+                            onChange={e => this.updateField(e)}
+                            placeholder="Digite o nome..." />
+                        
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o seu e-mail..." />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justifify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
